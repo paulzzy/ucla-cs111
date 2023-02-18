dae05d7994b4a78693020eac95b18114946f576e
Paul Zhang <paulz@ucla.edu>
2023-02-17 18:13:37 -0800

    Lab2: Implement round robin scheduling, without measuring

--- lab2/rr.c
+++ lab2/rr.c
@@ -25,6 +25,24 @@ struct process {
 
 TAILQ_HEAD(process_list, process);
 
+static int compare_by_arrival(const void *left, const void *right) {
+  u32 diff = ((struct process *)left)->arrival_time -
+             ((struct process *)right)->arrival_time;
+
+  // Stable sort by returning `left` less than `right` when they have equal
+  // arrival times, assuming that the array is sorted left-to-right
+  return (diff != 0) ? (int)diff : -1;
+}
+
+static struct process *remove_and_get_next(struct process_list *list,
+                                           struct process *current) {
+  struct process *next = TAILQ_NEXT(current, pointers);
+  TAILQ_REMOVE(list, current, pointers);
+  // printf("curr pid: %d\n", current->pid);
+  // printf("next pid: %d\n", next == NULL ? -1 : next->pid);
+  return next;
+}
+
 u32 next_int(const char **data, const char *data_end) {
   u32 current = 0;
   bool started = false;
@@ -135,6 +153,62 @@ int main(int argc, char *argv[]) {
   u32 total_response_time = 0;
 
   /* Your code here */
+  if (size > 0) {
+    u32 current_time = 0;
+    u32 run_time = 0;
+
+    u32 queue_idx = 0;
+
+    struct process *current = NULL;
+
+    // I swear to god this better be stable sort 🙏
+    qsort(data, size, sizeof(struct process), compare_by_arrival);
+
+    // Exit loop when all processes are done executing.
+    //
+    // "Runs" a process at the end, so all checking happens at the start
+    while (queue_idx < size || (queue_idx == size && !TAILQ_EMPTY(&list))) {
+      // Add new process to list if now is arrival time
+      if (queue_idx < size && data[queue_idx].arrival_time == current_time) {
+        TAILQ_INSERT_TAIL(&list, &data[queue_idx], pointers);
+        queue_idx++;
+
+        // Run new process if it's the only process
+        if (current == NULL) {
+          current = TAILQ_FIRST(&list);
+        }
+      }
+
+      // Switch to next process if current is finished
+      if (current->burst_time == 0) {
+        current = remove_and_get_next(&list, current);
+        run_time = 0;
+
+        // Update total response time if there are more runnable processes
+        // if (current != NULL) {
+        //   printf("pid %d arrived: %d, now: %d\n", current->pid,
+        //          current->arrival_time, current_time);
+        //   total_response_time += current_time - current->arrival_time;
+        // }
+      }
+
+      // Interrupt process when it uses up a quantum
+      if (run_time == quantum_length) {
+        struct process *timed_out_process = current;
+        current = remove_and_get_next(&list, current);
+        TAILQ_INSERT_TAIL(&list, timed_out_process, pointers);
+        run_time = 0;
+      }
+
+      // "Run" current process if it exists
+      if (current != NULL) {
+        current->burst_time--;
+        run_time++;
+      }
+      current_time++;
+    }
+  }
+
   /* End of "Your code here" */
 
   printf("Average waiting time: %.2f\n",
